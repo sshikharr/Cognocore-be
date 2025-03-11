b0cadf5827ef4c267ac414aee6d0e8ebabcfa71a
sshikharr <shikhardwivedi2019@gmail.com>
2025-03-11 13:26:46 +0530

    Changed URI connection logic

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Test route
+app.get('/', (req, res) => {
+  res.send('Backend is live');
+});
+
 // Routes
 app.use('/tasks', taskRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
